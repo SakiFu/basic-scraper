04679c2705756995be76e866bc671869077be849
Jim Grant <jimgrant@gmail.com>
2015-07-07 14:02:07 -0700

    implement get_inspection_page

--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+import requests
+
 INSPECTION_DOMAIN = 'http://info.kingcounty.gov'
 INSPECTION_PATH = '/health/ehs/foodsafety/inspections/Results.aspx'
 INSPECTION_PARAMS = {
@@ -18,3 +20,14 @@ INSPECTION_PARAMS = {
     'Fuzzy_Search': 'N',
     'Sort': 'B'
 }
+
+
+def get_inspection_page(**kwargs):
+    params = INSPECTION_PARAMS.copy()
+    for k, v in kwargs.items():
+        if k in INSPECTION_PARAMS:
+            params[k] = v
+    resp = requests.get(INSPECTION_DOMAIN + INSPECTION_PATH, params)
+    resp.raise_for_status()
+    return resp.content, resp.encoding
+
